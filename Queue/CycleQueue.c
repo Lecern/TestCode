25484c33253388ecf86f24d0deecff8d302bb8e3
jerry0319 <663794353@qq.com>
2017-06-22 16:56:26 +0800

    queue

--- Queue/CycleQueue.c
+++ Queue/CycleQueue.c
@@ -2,3 +2,32 @@
 // Created by Jerry on 2017/6/22 0022.
 //
 
+#include <stdio.h>
+
+#define MaxSize 50
+
+typedef int ElemType;
+typedef struct {
+    ElemType data[MaxSize];
+    int front;
+    int rear;
+}*CycleQueue;
+
+int EnCycleQueue(CycleQueue q, ElemType e) {
+    if ((q->rear + 1) % MaxSize == q->front) {
+        printf("队列满");
+        return 0;
+    }
+    q->rear = (q->rear + 1) % MaxSize;
+    q->data[q->rear] = e;
+    return 1;
+}
+
+ElemType DeCycleQueue(CycleQueue q) {
+    if (q->rear == q->front) {
+        printf("队列空");
+        return 0;
+    }
+    q->front = (q->front + 1) % MaxSize;
+    return q->data[q->front];
+}
